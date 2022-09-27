213737fd4c2ebe3f260b8c70ed99e8719db49212
Shuang Wu <seanwu1105@gmail.com>
2022-09-26 23:07:00 -0400

    build(lab3): add glad

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 int main() {
     std::cout << "hello world" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     return 0;
 }
