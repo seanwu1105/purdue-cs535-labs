7d9902de0bda7599d63bee9a0d18af30f026fab3
Shuang Wu <seanwu1105@gmail.com>
2022-09-26 23:22:00 -0400

    feat(lab3): show window

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -3,11 +3,47 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
 int main() {
-    std::cout << "hello world" << std::endl;
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Lab 3", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL()) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+        // TODO: input
+        
+        glClear(GL_COLOR_BUFFER_BIT);
+        // TODO: render
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
